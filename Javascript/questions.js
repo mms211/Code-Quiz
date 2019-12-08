8978f7f06089510c507789122d040f78b8e53cb8
Morgan Sommer <morgana.sommer@gmail.com>
2019-12-08 18:24:13 -0500

    List of quiz questions completed

---
+++ Javascript/questions.js
@@ -0,0 +1,42 @@
+var questions = [
+  {
+    title: "All the following musicals are by Andrew Lloyd Webber EXCEPT:",
+    choices: ["Phantom of the Opera", "Evita", "Les Miserables", "Joseph and the Amazing Technicolor Dreamcoat"],
+    answer: "Les Miserables"
+  },
+  {
+    title: "Who won the 2019 Tony for Best Musical?",
+    choices: ["Hadestown", "Tootsie", "Dear Evan Hansen", "The Cher Show"],
+    answer: "Hadestown"
+  },
+  {
+    title: "In which city does The Phantom of the Opera take place?",
+    choices: ["Vienna, Austria", "Paris, France", "London, England", "Berlin, Germany"],
+    answer: "Paris, France"
+  },
+  {
+    title: "What is the name of The Wicked Witch of the West in the Broadway musical Wicked?",
+    choices: ["Brunhilda", "Galinda", "Nessa", "Elphaba"],
+    answer: "Elphaba"
+  },
+  {
+    title: "Which of the following musicals is NOT based on a movie?",
+    choices: ["Waitress", "Be More Chill", "Kinky Boots", "Beetlejuice"],
+    answer: "Be More Chill"
+  },
+  {
+    title: "Which musical artist has NOT had their music made into a Broadway musical?",
+    choices: ["Pink Floyd", "Jimmy Buffet", "Carol King", "ABBA"],
+    answer: "Pink Floyd"
+  },
+  {
+    title: "Hamilton creator, Lin-Manuel Miranda, has won which of the following awards?",
+    choices: ["Tony", "Grammy", "Pulitzer Prize", "All of the above"],
+    answer: "All of the above"
+  },
+  {
+    title: "As of 2019, which is the longest running musical on Broadway?",
+    choices: ["Chicago", "Lion King", "The Phantom of the Opera", "CATS"],
+    answer: "The Phantom of the Opera"
+  },
+];
