5b4375fbe6beadf0b3024fff78717fa412d9187f
Morgan Sommer <morgana.sommer@gmail.com>
2019-12-11 22:06:00 -0500

    completed assignment

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     // global variables
 
     var quizIntro = $("#intro");
-    var startQuiz = $("#start-quiz");
     var quizCont = $("#quiz");
     var wrongCorrect = $("#wrong-correct");
     var pos = 0;
@@ -15,34 +14,39 @@ $(document).ready(function () {
     var chB = questions[pos].choices[1];
     var chC = questions[pos].choices[2];
     var chD = questions[pos].choices[3];
-    console.log(chA, chB, chC, chD);
+    
+    // create landing screen
 
+    function landingScreen() {
+        quizCont.html($("<h1>Broadway Quiz Challenge</h1>"));
+        quizCont.append($("<p>Try to answer the following Broadway related questions within the time limit. Keep in mind that incorrect answers will penalize your score time by 10 seconds!</p>"));
+        quizCont.append($("<button type=button id=start-quiz>Start Quiz</button>"));
+    }
+    
     // create timer
 
     function createTimer() {
-        var timer = $("#timer");
+        var timer = $("#timer");    
         timer.html("Time: 1:30");
 
-        var interval = setInterval(function timeIt() {
+        interval = setInterval(function () {
             counter++;
             timer.html("Time: " + convertSeconds(timeleft - counter));
             return;
         }, 1000);
-
-        function convertSeconds(s) {
-            var min = Math.floor(s / 60);
-            var sec = s % 60;
-            sec = sec.toString().padStart(2, "0");
-            if (min <= 0 && Number(sec) <= 0) {
-                clearInterval(interval);
-                createScoreForm();
-            } else if (pos > questions.length) {
-                clearInterval(interval);
-            } else {
-                return min + ":" + sec;
-            };
-        };
     }
+
+    function convertSeconds(s) {
+        var min = Math.floor(s / 60);
+        var sec = s % 60;
+        sec = sec.toString().padStart(2, "0");
+        if (timeleft - counter <= 0) {
+            clearInterval(interval);
+            createScoreForm();
+        } else {
+            return min + ":" + sec;
+        };
+    };
 
     // create quiz structure
 
@@ -80,13 +84,24 @@ $(document).ready(function () {
 
     function createHighScorePage(event) {
         quizCont.html($("<h2>High Scores</h2>"));
-    }
+        var name = localStorage.getItem("initial");
+        var highScore = localStorage.getItem("score");
+        quizCont.append($("<p class=high-score>" + name + " - " + highScore + "</p>"));
+        var button1 = $("<button>");
+        var button2 = $("<button>");
+        var goBackButton = button1.attr("id", "go-back");
+        goBackButton.text("Go Back");
+        var clearButton = button2.attr("id", "clear");
+        clearButton.text("Clear High Scores")
+        quizCont.append(goBackButton);
+        quizCont.append(clearButton);
+    };
 
     // store to local storage
 
     function storeVars() {
         var initialEntry = $("#initial");
-        localStorage.setItem("initial", initialEntry.value);
+        localStorage.setItem("initial", initialEntry.val());
         localStorage.setItem("score", score);
     }
 
@@ -95,8 +110,8 @@ $(document).ready(function () {
     function buttonID() {
 
         var id = $(this).attr("id");
-        console.log(id)
-        var isCorrect = checkAnswer(id)
+        console.log(id);
+        var isCorrect = checkAnswer(id);
 
         if (isCorrect) {
             console.log("Correct");
@@ -108,6 +123,8 @@ $(document).ready(function () {
                     createQuiz(pos);
                 } else {
                     createScoreForm();
+                    clearInterval(interval);
+                    pos = 0;
                 }
             }, 500)
         } else {
@@ -122,18 +139,18 @@ $(document).ready(function () {
     function checkAnswer(id) {
         if (questions[pos].choices[Number(id)] === questions[pos].answer) {
             console.log("CORRECT!");
-            return true
+            return true;
         }
         console.log("INCORRECT!");
-        return false
+        return false;
     };
 
     // call functions
-
+    // landing screen
+    landingScreen();
+    
     // start quiz
-    startQuiz.click(function () {
-        quizIntro.attr("class", "hidden");
-        $("#quiz-div").removeClass("hidden");
+    $("body").on("click", "#start-quiz", function () {
         createTimer();
         createQuiz(pos);
     });
@@ -151,10 +168,24 @@ $(document).ready(function () {
     // view high scores
     $("body").on("click", "#view-high-score", function () {
         event.preventDefault();
+        clearInterval(interval);
         quizIntro.attr("class", "hidden");
         wrongCorrect.attr("class", "hidden");
         $("#quiz-div").removeClass("hidden");
         createHighScorePage();
     });
+
+    // go back to quiz
+    $("body").on("click", "#go-back", function () {
+        event.preventDefault();
+        landingScreen();
+    });
+
+    // clear button
+    $("body").on("click", "#clear", function () {
+        event.preventDefault();
+        $(".high-score").remove();
+        localStorage.clear();
+    })
 
 });
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Code-Quiz
+
+## Description
+
+This is a Broadway themed quiz game that was created to provide entertainment and test a user's Broadway knowledge. This game works as follows:
+
+- 8 broadway questions, each containing 4 choices
+- Starting timer of 90secs (1:30)
+- Each correct answer progresses the quiz
+- Each incorrect answer deducts 10secs from the timer
+- Quiz ends when each questions is correctly answered OR if the timer hits 0
+- Quiz is scored as 1 point for each remaining second on the timer
+
+The user is then able to enter their initials and scores into the High Scores page.
+
+## Link
+
+https://mms211.github.io/Code-Quiz/
+
+## License
+
+Copyright (c) 2019 Morgana Sommer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
